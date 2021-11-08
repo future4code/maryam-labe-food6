3fc0035378cb33a5420763289937b99bafec1af9
Murilo Terenciani <murilo.terenciani@gmail.com>
2021-11-08 18:14:02 -0300

    Arrumadin quando atualiza

--- src/pages/EditUserPage/FormEditUser.js
+++ src/pages/EditUserPage/FormEditUser.js
@@ -1,6 +1,8 @@
 import { Button, CircularProgress, TextField } from "@material-ui/core";
-import React, { useContext, useState } from "react";
+import axios from "axios";
+import React, { useContext, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
+import { headers, URL_Base } from "../../constants/urls";
 import { GlobalContext } from "../../contexts/GlobalContext";
 import useForm from "../../hooks/useForm";
 import { updateProfile } from "../../services/user";
@@ -10,14 +12,33 @@ const FormEditUser = () => {
 
     const history = useHistory()
 
-    const { states, setters, requests } = useContext(GlobalContext)
-    const {profile} = requests
+    // const { states, setters, requests } = useContext(GlobalContext)
+    // const {profile} = requests
 
-    const [form, onChange, clear] = useForm({
-        name: `${profile.name}`,
-        email: `${profile.email}`,
-        cpf: `${profile.cpf}`,
+    const [form, onChange, clear, setForm] = useForm({
+        name: "",
+        email: "",
+        cpf: "",
     })
+
+    useEffect(() => {
+        profile()
+    }, [])
+
+    const profile = () => {
+        axios
+        .get(`${URL_Base}/profile`, headers)
+        .then((res) => {
+            setForm({
+                name: res.data.user.name,
+                email: res.data.user.email,
+                cpf: res.data.user.cpf,
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
 
     const [isLoading, setIsLoading] = useState(false)
 
