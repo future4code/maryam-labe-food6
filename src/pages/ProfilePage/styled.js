3c422188b472d41b84c21f3859e5516cd8a79dcc
Sarah Romanhol Falconiere <sarahromanhol@gmail.com>
2021-11-12 17:00:21 -0300

    Historico de pedidos pronto. Falta testar com os pedidos

--- src/pages/ProfilePage/styled.js
+++ src/pages/ProfilePage/styled.js
@@ -90,3 +90,52 @@ export const OrderHistorys = styled.div `
         color: black;
     }
 `
+
+export const OrderHistoryCard = styled.div`
+    display: flex;
+    flex-direction: column;
+    padding: 1rem;
+    height: 5.12rem;
+    margin: 1rem auto;
+    border-radius: 8px;
+    border: solid 1px #b8b8b8;
+        p:nth-child(1){
+        width: 296px;
+        height: 18px;
+        margin: 0 0 9px;
+        font-family: Roboto;
+        font-size: 16px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.39px;
+        color: #e86e5a;
+        }
+        p:nth-child(2){
+        width: 296px;
+        height: 18px;
+        margin: 9px 0 7px;
+        font-family: Roboto;
+        font-size: 12px;
+        font-weight: normal;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.29px;
+        color: #000;
+        }
+        p:nth-child(3){
+        width: 296px;
+        height: 18px;
+        margin: 7px 0 0;
+        font-family: Roboto;
+        font-size: 16px;
+        font-weight: bold;
+        font-stretch: normal;
+        font-style: normal;
+        line-height: normal;
+        letter-spacing: -0.39px;
+        color: #000;
+        }
+`
